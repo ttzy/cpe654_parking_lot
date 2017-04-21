5ff7941655e72abbe6f8fed713b1f48a904e4056
Sudhansh Aggarwal <sudhansh@sudhansh.com>
2017-04-21 14:59:12 -0400

    added functionality to save the centres

--- parking_lot.py
+++ parking_lot.py
@@ -16,9 +16,32 @@ loc = np.where( res >= threshold)
 #Y is total if difference is small enough
 #count is counter
 
+total = 1
+xs = {}
+ys = {}
+count=1
+
 for pt in zip(*loc[::-1]):
     #cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (255,255,255), 2)
     cX = int(pt[0] + w/2)
     cY = int(pt[1] + h/2)
-    cv2.circle(img_rgb, (cX, cY), 7, (255, 255, 255), -1)
+	# get cx and cy from the previous code
+	if count == 1 or (abs(cX - cxp) <=10 and abs(cY - cyp) <=10):
+		x = x + cX
+		y = y + cY
+		cxp = cX
+		cyp = cY
+		count = count + 1
+
+	elif abs(cx - cxp) >=15 and abs(cy - cyp) >=20:
+		if count > 1:
+			count = count - 1
+		xs = {total : (x/count)}
+		ys = {total : (y/count)}
+		# draw circle at 
+		count = 1
+		total = total + 1
+		x = cxp = cx
+		y = cyp = cy
+    # cv2.circle(img_rgb, (cX, cY), 7, (255, 255, 255), -1)
 cv2.imwrite('res.png',img_rgb)
